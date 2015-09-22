19f1c5fe254141a06f55410bbb5effd7a11291e2
Dominik Enßle <dominik.enssle@innoq.com>
2015-09-22 12:34:21 +0200

    Userpage Init

---
+++ app/src/main/java/de/hhbk/de/cb/UserActivity.java
@@ -0,0 +1,35 @@
+package de.hhbk.de.cb;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+/**
+ * Created by admin on 22.09.15.
+ */
+public class UserActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        setContentView(R.layout.activity_user);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.dropdown_menue, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        DropdownMenue menue = new DropdownMenue();
+        int id = item.getItemId();
+        return menue.manageMenue(id, this);
+    }
+}
