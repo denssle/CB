b37b2cfd840e72051f9ede72410eaed70d905a7f
Dominik Enßle <dominik.enssle@web.de>
2015-10-13 13:43:47 +0200

    SchoolDay Init

---
+++ app/src/main/java/de/hhbk/de/cb/model/SchoolDay.java
@@ -0,0 +1,16 @@
+package de.hhbk.de.cb.model;
+
+import java.util.Date;
+
+/**
+ * Created by dominik on 13.10.15.
+ */
+public class SchoolDay {
+    private Date date;
+    private Schoolclass schoolclass;
+
+    public SchoolDay() {
+    }
+
+
+}
